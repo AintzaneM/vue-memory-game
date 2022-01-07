52a650f4ba9ef73540414af7046bfbb173585581
Aintzane Morales <aintzanemorales1992@gmail.com>
2022-01-07 15:11:43 +0100

    refactor card component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
   <h1>Memory Game</h1>
   <section class="board">
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
+    <Card v-for="(card, index) in cardList" 
+    :key="`card-${index}`" 
+    :value="card" />
+    
   </section>
+  
   
 </template>
 
 <script>
 
+import Card from './components/Card.vue';
 
 export default {
   name: 'App',
-  
+  components: {
+    Card
+  },
+  setup() {
+    const cardList = []
+    for (let i = 0; i < 12; i++) {
+      cardList.push(i)
+    }
+    return {
+      cardList
+    }
+  }
 }
 </script>
 
 <style>
 @import './assets/App.css';
-
 </style>
